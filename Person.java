7efbd08db1692b091331cf03e5b48bb037a186d8
s4095526 <s4095526@student.rmit.edu.au>
2025-05-24 10:22:14 +1000

    Added the code outline

---
+++ Person.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Date;
+
+public class Person {
+    private String personID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String birthDate;
+    private HashMap<Date, Integer> demeritPoints;
+    private boolean isSuspended;
+
+
+    public boolean addPerson() {
+        //
+        return true;
+    }
+    
+    public static void updatePersonalDetails() {
+    
+    }
+
+    public String addDemeritPoints() {
+
+        return "Success";
+    }
+}
